464af47b847625311ceb4c6b91d92ce54bc59cee
Lucas <lukin@192.168.56.1>
2019-11-06 00:32:22 -0200

    Atualizado métodos de pagamento.

--- src/dao/VendaDAO.java
+++ src/dao/VendaDAO.java
@@ -11,14 +11,11 @@ import java.sql.Connection;
 import java.sql.Date;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
-import java.sql.ResultSetMetaData;
 import java.sql.SQLException;
 import java.time.LocalDate;
-import java.util.Collections;
-import java.util.Comparator;
-import java.util.List;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+import javax.swing.JOptionPane;
 import javax.swing.JTable;
 import javax.swing.table.DefaultTableModel;
 import model.Cliente;
@@ -48,74 +45,75 @@ public class VendaDAO {
         VendaDAO.connection=ConnectionFactory.getConnection();
     }
     
-    public static double updateVenda(double pagoAgora, int idVenda, double valorTotal, double valorPago, int quantiaParcelas, int parcelasPagas, Date dataVencimento, java.sql.Date dataNVencimento, boolean cobrarJuros, boolean attTodas) {
+    public static void updateVenda(double pagoAgora, int idVenda, double valorTotal, double valorPago, int quantiaParcelas, int parcelasPagas, Date dataVencimento, java.sql.Date dataNVencimento, boolean cobrarJuros, boolean attTodas) {
         String sql = "UPDATE tryfatus.vendas SET valorTotal = ?, valorPago = ?, quantiaParcelas = ?, dataVencimento = ?, ativa=? WHERE idVenda = ?";
         double pagAgora = pagoAgora;
         
         //
         try {
             PreparedStatement stmt = connection.prepareStatement(sql);
-            boolean serAtiva = true; 
-            
             //
             double valorTot = valorTotal;
             double valJuros = 0;
             if (dataVencimento.before(DateConvert.LocalToUtil(LocalDate.now()))) {
-                valJuros = JurosCalc.Juros(valorTotal/parcelasPagas, JurosDAO.pegarJuros(), dataVencimento);
+                valJuros = JurosCalc.Juros((valorTotal-valorPago)/quantiaParcelas, JurosDAO.pegarJuros(), dataVencimento);
             }
-            if (cobrarJuros=true) {
+            if (cobrarJuros==true) {
               valorTot=valorTot+valJuros;
             } 
             stmt.setDouble(1, valorTot);
             
             //
             double valPag = 0;
-            if (pagAgora>valorTot) {
-                valPag=valorTot;
-                pagAgora=pagAgora-valorTot;
-                serAtiva = false;
-            }
-            if (pagAgora==valorTot) {
-                valPag=valorTot;
-                pagAgora=0;
-                serAtiva=false;
-            }
-            if (pagAgora<valorTot) {
-                valPag=valorPago+pagAgora;
+            if (pagoAgora>0) {
+                if (pagAgora>valorTot) {
+                    valPag=valorTot;
+                    pagAgora=pagAgora-valorTot;
+                }
+                if (pagAgora==valorTot) {
+                    valPag=valorTot;
+                    pagAgora=0;
+                }
+                if (pagAgora<valorTot) {
+                    valPag=valorPago+pagAgora;
+                    pagAgora=0;
+                }
+            } else {
+                valPag=valorPago;
             }
             stmt.setDouble(2, valPag);
             
             //
-            if (quantiaParcelas-parcelasPagas<=0) {
-                stmt.setInt(3, 1);
+            if (pagoAgora>0 || attTodas==true) {
+                if (quantiaParcelas-parcelasPagas<=0) {
+                    stmt.setInt(3, 1);
+                } else {
+                    stmt.setInt(3, quantiaParcelas-parcelasPagas);
+                }
             } else {
-                stmt.setInt(3, quantiaParcelas-parcelasPagas);
+                stmt.setInt(3, quantiaParcelas);
             }
             
             //
             if (pagAgora>0) {
                 stmt.setDate(4, dataNVencimento);
-            } else if (attTodas=true) {
+            } else if (attTodas==true) {
                 stmt.setDate(4, dataNVencimento);
             } else {
                 stmt.setDate(4, dataVencimento);
             }
             
             //
-            if (serAtiva=true) {
-                stmt.setInt(5, 1);
-            } else {
-                stmt.setInt(5, 0);
-            }
+            stmt.setInt(5, valPag>=valorTot ? 0 : 1);
             
             //
             stmt.setInt(6, idVenda);
             stmt.executeUpdate();
-            return pagAgora;
+            //return pagAgora;
         } catch (SQLException ex) {
             ex.printStackTrace();
         }
-        return 0;
+        //return 0;
     }
     public static void updateVendas(double valorPago, Cliente cliente, int parcelasPagas, java.sql.Date novoVencimento, boolean cobrarJuros, boolean attTodas) {
         String sql = "SELECT idVenda,valorTotal,valorPago,quantiaParcelas,dataVencimento FROM tryfatus.vendas WHERE Clientes_CPF='"+cliente.getCPF()+"' AND ativa=1 ORDER BY dataVencimento";
@@ -123,20 +121,58 @@ public class VendaDAO {
             PreparedStatement stmt = connection.prepareStatement(sql);
             ResultSet rs = stmt.executeQuery();
             double a = valorPago;
+            double valJuros=0;
             while(rs.next()) {
-                if (a>0 || attTodas==true) {
-                    a=a-updateVenda(valorPago, rs.getInt("idVenda"), rs.getDouble("valorTotal"), rs.getDouble("valorPago"), rs.getInt("quantiaParcelas"), parcelasPagas, rs.getDate("dataVencimento"), novoVencimento, cobrarJuros, attTodas);
+                if (a>0) {
+                    if (rs.getDate("dataVencimento").before(DateConvert.LocalToUtil(LocalDate.now()))) {
+                        valJuros = JurosCalc.Juros((rs.getDouble("valorTotal")-rs.getDouble("valorPago"))/rs.getInt("quantiaParcelas"), JurosDAO.pegarJuros(), rs.getDate("dataVencimento"));
+                    }
+                    double aberto=rs.getDouble("valorTotal")+valJuros-rs.getDouble("valorPago");
+                    if (aberto>=a) {
+                        updateVenda(a, rs.getInt("idVenda"), rs.getDouble("valorTotal"), rs.getDouble("valorPago"), rs.getInt("quantiaParcelas"), parcelasPagas, rs.getDate("dataVencimento"), novoVencimento, cobrarJuros, attTodas);
+                        a=0;
+                    }
+                    else if (aberto<a) {
+                        updateVenda(aberto, rs.getInt("idVenda"), rs.getDouble("valorTotal"), rs.getDouble("valorPago"), rs.getInt("quantiaParcelas"), parcelasPagas, rs.getDate("dataVencimento"), novoVencimento, cobrarJuros, attTodas);
+                        a=a-aberto;
+                    }
+                    
+                } else if (attTodas==true){
+                    if (rs.getDate("dataVencimento").before(novoVencimento)) {
+                        int par = rs.getInt("quantiaParcelas")-parcelasPagas;
+                        if (par<1) {
+                            par=1;
+                        }
+                        updateVencimento(rs.getInt("idVenda"), novoVencimento, par, rs.getDouble("valorTotal")+valJuros);
+                    }
                 } else {
                     break;
                 }
-                System.out.println(a);
             }
+            if (a>0) {
+                JOptionPane.showMessageDialog(null, "Sobraram "+a+"R$ de troco!");
+            }
+            stmt.close();
         } catch (SQLException ex) {
             ex.printStackTrace();
         }
         //return list;
     }
     
+    public static void updateVencimento(int idVenda, java.sql.Date dataVencimento, int parcelas, double juros) {
+        String sql = "UPDATE tryfatus.vendas SET dataVencimento = ?, quantiaParcelas = ?, valorTotal = ? WHERE idVenda = ?";
+        try {
+            PreparedStatement stmt = connection.prepareStatement(sql);
+            stmt.setDate(1, dataVencimento);
+            stmt.setInt(2, parcelas);
+            stmt.setDouble(3, juros);
+            stmt.setInt(4, idVenda);
+            stmt.executeUpdate();
+            stmt.close();
+        } catch (SQLException ex) {
+            Logger.getLogger(VendaDAO.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
     public static void add(Venda venda) {
         String sql = "INSERT INTO vendas(Clientes_CPF,Produto,valorTotal,valorPago,quantiaParcelas,metodoPagamento,dataCompra,dataVencimento,ativa) VALUES(?,?,?,?,?,?,?,?,?)";
         try {
